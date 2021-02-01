4bcfa3a504327fd47b49cbb3ffe9de783007db1c
Mankowski92 <mankowski92@gmail.com>
2021-02-01 21:57:52 +0100

    heroku

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+(EMAIL = testmail03072020),
+  (PASS = PaSS1234),
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+app.post("/send", (req, res) => {
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+    console.log(data);
+    const mail = {
+      sender: `${data.name} <${data.email}>`,
+      to: process.env.EMAIL, // receiver email,
+      subject: "New contact request",
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
+
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
